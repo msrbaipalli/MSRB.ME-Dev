484e89f4b5c09fb3111175eed74c9640244535d1
Manga Baipalli <manga.baipalli@gmail.com>
2020-10-13 16:28:44 -0400

    feat(certificates): update certificates component

--- src/app/modals/long-story-short/certificates/certificates.component.ts
+++ src/app/modals/long-story-short/certificates/certificates.component.ts
@@ -1,4 +1,7 @@
+import { isNull } from '@angular/compiler/src/output/output_ast';
 import { Component, OnInit } from '@angular/core';
+import { isNullOrUndefined } from 'src/app/shared/utils/utils.service';
+import { CERTIFICATE_LIST } from './certificates.constants';
 
 @Component({
   selector: 'app-certificates',
@@ -6,10 +9,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./certificates.component.css']
 })
 export class CertificatesComponent implements OnInit {
+  certificateList = CERTIFICATE_LIST;
 
   constructor() { }
 
   ngOnInit() {
   }
 
+  onViewCertificateClick(url: string): void {
+    if (isNullOrUndefined(url)) {
+      return;
+    }
+
+    window.open(url, '_blank');
+  }
 }
--- src/app/projects/stocks-watch/stocks-watch.component.ts
+++ src/app/projects/stocks-watch/stocks-watch.component.ts
@@ -2,6 +2,7 @@ import {Component, OnInit} from '@angular/core';
 import {FormControl} from '@angular/forms';
 import {Observable} from 'rxjs';
 import {map, startWith} from 'rxjs/operators';
+import {CdkDragDrop, moveItemInArray, transferArrayItem} from '@angular/cdk/drag-drop';
 
 @Component({
   selector: 'app-stocks-watch',
@@ -9,13 +10,29 @@ import {map, startWith} from 'rxjs/operators';
   styleUrls: ['./stocks-watch.component.css']
 })
 export class StocksWatchComponent implements OnInit {
-  boxes = [
-    {
-      title: 'Hi'
-    },
-    {
-      title: 'Boys!'
+todo = [
+  'Get to work',
+  'Pick up groceries',
+  'Go home',
+  'Fall asleep'
+];
+
+done = [
+  'Get up',
+  'Brush teeth',
+  'Take a shower',
+  'Walk dog'
+];
+
+drop(event: CdkDragDrop<string[]>) {
+    if (event.previousContainer === event.container) {
+      moveItemInArray(event.container.data, event.previousIndex, event.currentIndex);
+    } else {
+      transferArrayItem(event.previousContainer.data,
+                        event.container.data,
+                        event.previousIndex,
+                        event.currentIndex);
     }
-  ];
+  }
   ngOnInit() {}
 }
